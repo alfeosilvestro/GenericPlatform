8105da3690d57103f65c66afbc9aac8c29cee6c2
Alex_Development_Machine <alfeo.silvestro@live.com>
2018-03-30 16:32:10 +0800

    sys_file_system table create project

--- docker_development_environment/Db_Updates/20180330_update.sql
+++ docker_development_environment/Db_Updates/20180330_update.sql
@@ -1,1 +1,22 @@
-﻿
+﻿/* Changes made by AMK - Start */
+
+CREATE TABLE `platform_db`.`sys_file_system` (
+  `Id` INT NOT NULL,
+  `FileName` VARCHAR(100) NOT NULL,
+  `FileDescription` VARCHAR(100) NULL,
+  `OriginalFileName` VARCHAR(100) NOT NULL,
+  `FileContent` LONGTEXT NULL,
+  `ApplicationId` VARCHAR(100) NULL,
+  `Reference1` VARCHAR(100) NULL,
+  `Reference2` VARCHAR(100) NULL,
+  `Reference3` VARCHAR(100) NULL,
+  `CreatedDate` DATETIME NULL,
+  `CreatedBy` VARCHAR(45) NULL,
+  `UpdatedDate` DATETIME NULL,
+  `UpdatedBy` VARCHAR(45) NULL,
+  `IsActive` BIT(1) NULL DEFAULT b'1',
+  `Version` VARCHAR(45) NULL,
+  PRIMARY KEY (`Id`));
+
+  /* Changes made by AMK - End */
+
