eeaf1a9cbacc9bb205bfdbfab92f593069ccff91
soseedytu <30871297+soseedytu@users.noreply.github.com>
2018-03-30 15:51:10 +0800

    Sys_User, Sys_User_Role initial check in.

---
+++ Com.PlatformServices.Common/DAL/Entities/SystemUser/Sys_User.cs
@@ -0,0 +1,28 @@
+﻿using Com.PlatformServices.Common.FoundationClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using Com.PlatformServices.Common.DAL.Entities;
+
+namespace Com.PlatformServices.Common.DAL.Entities.SystemUser
+{
+    [Table("Sys_User")]
+    public class Sys_User : BaseEntity
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(18, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [RegularExpression(@"^((?=.*[a-z])(?=.*[A-Z])(?=.*\d)).+$", ErrorMessage = "Password doesn't meet the requirements")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        public string Active { get; set; }
+
+        public ICollection<Sys_User_Role> UserRoles { get; set; }
+    }
+}
---
+++ Com.PlatformServices.Common/DAL/Entities/SystemUser/Sys_User_Role.cs
@@ -0,0 +1,19 @@
+﻿using Com.PlatformServices.Common.FoundationClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Com.PlatformServices.Common.DAL.Entities.SystemUser
+{
+    [Table("Sys_User_Role")]
+    public class Sys_User_Role : BaseEntity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        [ForeignKey("UserID")]
+        public Sys_User user { get; set; } 
+
+    }
+}
