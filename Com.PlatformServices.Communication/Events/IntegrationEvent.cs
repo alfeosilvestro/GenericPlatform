1af09ea928148e6d7f70f7d9c7f14c2165d79d16
Alex_Development_Machine <alfeo.silvestro@live.com>
2018-04-03 11:22:43 +0800

    RabbitMQ EventBus initial checkin

---
+++ Com.PlatformServices.Communication/Events/IntegrationEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Com.PlatformServices.Communication.Events.Declaration;
+
+namespace Com.PlatformServices.Communication.Events
+{
+    public class IntegrationEvent
+    {
+        public IntegrationEvent()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public Guid Id { get; }
+        public DateTime CreationDate { get; }
+    }
+}
