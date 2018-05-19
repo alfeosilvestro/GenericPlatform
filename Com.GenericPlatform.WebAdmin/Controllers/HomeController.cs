5237fb49b9830f9c361d7ff010e7618fdd1cb170
Alex_Development_Machine <alfeo.silvestro@live.com>
2018-05-19 23:15:27 +0800

    Web Admin Project Inital Check in

---
+++ Com.GenericPlatform.WebAdmin/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Com.GenericPlatform.WebAdmin.Models;
+using Com.GenericPlatform.Services.Code;
+
+namespace Com.GenericPlatform.WebAdmin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ICodesService svs;
+
+        public HomeController(ICodesService svs)
+        {
+            this.svs = svs;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await svs.GetCodesByPage("", 1);
+
+            return View(result);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Com.GenericPlatform.WebAdmin/Startup.cs
@@ -0,0 +1,78 @@
+﻿using Com.EzTender.Repository.Context;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Com.EzTender.Repository;
+using Com.EzTender.Common;
+using Com.GenericPlatform.Services.Code;
+
+namespace Com.GenericPlatform.WebAdmin
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            // Use a PostgreSQL database
+            var sqlConnectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            // TODO: Update in each application
+            services.AddDbContext<DatabaseContext>(options =>
+                    options.UseMySQL(
+                    sqlConnectionString,
+                    b => b.MigrationsAssembly("Com.PlatformServices.SystemSettings") // main project name = Application Specific
+                )
+            );
+
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+
+            RegisterForDependencyInjection(services);
+        }
+
+        public void RegisterForDependencyInjection(IServiceCollection services)
+        {
+            //// Register for repository
+            services.AddScoped<ICodeRepository, CodeRepository>();
+            //services.AddScoped<GetMeToMyMechanic.Repository.Declaration.ILocationInformationRepository, GetMeToMyMechanic.Repository.Definition.LocationInformationRepository>();
+
+            //// Register for library
+            services.AddScoped<ICodesService, CodesService>();
+            //services.AddScoped<GetMeToMyMechanic.Lib.Declaration.ILocationInformationService, GetMeToMyMechanic.Lib.Definition.LocationInformationService>();
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
