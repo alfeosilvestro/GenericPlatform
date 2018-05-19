d66e878352bf011696d242b845bad45f0254635d
Alex_Development_Machine <alfeo.silvestro@live.com>
2018-05-19 23:50:46 +0800

    Initial checkin for: Route to user Login page as default Updated Empty Layout System user view model user controller Sigin view

--- Com.GenericPlatform.WebApp/Startup.cs
+++ Com.GenericPlatform.WebApp/Startup.cs
@@ -75,7 +75,7 @@ namespace Com.GenericPlatform.WebApp
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=User}/{action=SignIn}/{id?}");
             });
         }
     }
